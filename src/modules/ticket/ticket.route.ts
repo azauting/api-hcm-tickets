60979310cbe05b2e47b1f31663a5491aab2a4cd3
SebastianG <sebastianale69@gmail.com>
2025-11-21 14:51:19 -0300

    verificacion de endpoints

--- src/modules/ticket/ticket.route.ts
+++ src/modules/ticket/ticket.route.ts
@@ -10,7 +10,7 @@ const router = express.Router();
 // ! REVISAR SI TIENEN AUDITORIA/LOGS
 // ! REVISAR SI FUNCIONAN CORRECTAMENTE
 // TODO: crear ticket - estado: ✅
-router.post('/tickets', verifyToken, ticketController.createTicket); 
+router.post('/tickets', verifyToken, ticketController.createTicket);
 // TODO: crear detalle observacion  - estado: ✅ (falta desarrollar)
 router.post('/tickets/:id/detalle/observacion', verifyToken, checkRole(['soporte', 'administrador']), ticketController.addTicketObservation);
 // TODO: crear detalle integrante - estado : ✅ (falta revisar)
@@ -18,29 +18,27 @@ router.post('/tickets/:id/detalle/integrante', verifyToken, checkRole(['soporte'
 
 // * PATCH Routes
 // ! REVISAR SI TIENEN AUDITORIA/LOGS
-// ! REVISAR SI FUNCIONAN CORRECTAMENTE
 // TODO: revisar ticket por el admin - estado: ✅
-router.patch('/tickets/:id/review',verifyToken,  checkRole(['administrador']),  ticketController.updateTicketAdmin); 
+router.patch('/tickets/:id/review', verifyToken, checkRole(['administrador']), ticketController.updateTicketAdmin);
 // TODO: asignar ticket por el admin o soporte - estado: ✅
-router.patch('/tickets/:id/assign',verifyToken,checkRole(['soporte', 'administrador']),ticketController.assignTicket);
-// TODO: cambios del ticket por parte del soporte - administrador - estado: ✅ (falta desarrollar)
+router.patch('/tickets/:id/assign', verifyToken, checkRole(['soporte', 'administrador']), ticketController.assignTicket);
+// TODO: cambios del ticket por parte del soporte - administrador - estado: ✅ 
 router.patch('/tickets/:id/update', verifyToken, checkRole(['soporte', 'administrador']), ticketController.updateTicketSupport);
 
 // * GET Routes
 // ! REVISAR SI FUNCIONAN CORRECTAMENTE
-// TODO: ruta para ver los tickets sin revisar - estado : ✅ (falta revisar)
+// TODO: ruta para ver los tickets sin revisar - estado : ✅ (funciona)
 router.get('/tickets/sin-revisar', verifyToken, checkRole(['administrador']), ticketController.getUnreviewedTickets);
 // TODO : ruta para ver mis tickets creados - estado: ✅
 router.get('/tickets/mis-tickets', verifyToken, ticketController.getTicketsByUserId);
 // TODO: ruta para ver un ticket especifico - pendiente a revision
 router.get('/tickets/:id', verifyToken, ticketController.getTicketById);
-// TODO: ruta para ver los tickets por unidad - estado: : ✅
+// TODO: ruta para ver los tickets por unidad - estado: : ✅ (desarrollar)
 router.get('/tickets/revisados/:unidad_id', verifyToken, checkRole(['administrador', 'soporte']), ticketController.getTicketsByUnitId);
 
 
 // rutas para obtener los tipos: 
-// todo: rutas para ver los tipos listas
-
+// todo: rutas para ver los tipos listas - estado: ✅
 router.get('/tickets/tipo_estado', verifyToken, ticketController.getStatusType);
 router.get('/tickets/tipo_prioridad', verifyToken, ticketController.getPriorityType);
 router.get('/tickets/tipo_origen', verifyToken, ticketController.getOriginType);
